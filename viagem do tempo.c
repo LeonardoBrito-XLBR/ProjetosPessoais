394dc359c7ecf778d98d890e6bc106915d115685
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-03-17 20:54:00 -0300

    Add files via upload

---
+++ viagem do tempo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+//CRIANDO ALGUNS TITULOS DO CODIGO
+
+void inicio (){
+	printf ("========================================");
+	printf ("\n===== SAUDAÇÕES VIAJANTE DO TEMPO =====");
+	printf ("\n========================================");
+	}
+
+void fim (){
+
+	printf ("========================================");
+	printf ("\n===== ATÉ MAIS CEDO VIAJANTE DO TEMPO =====");
+	printf ("\n========================================");
+	}	
+
+
+	int main (){
+		setlocale (LC_ALL, "portuguese"); //APLICANDO A BIBLIOTECA DAS ACENTUAÇÕES
+		
+		inicio ();
+		
+		char nomes [200][5]; //DECLARANDO VETOR = A QUANTIDADE DE CARACTERES E QUANTOS VARIAVEIS TEM 5
+		int i;
+		
+		for (i=0; i<5; i++){
+			printf ("\n\nDigite o nome do %dª Piloto: ", i+1);
+			scanf ("%d", &nomes[i]);
+			fflush (stdin);
+		}
+		
+	}	
