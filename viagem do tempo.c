5fbd170f044102da66681a22609404d21064b995
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-03-18 09:11:50 -0300

    Update viagem do tempo.c

--- viagem do tempo.c
+++ viagem do tempo.c
@@ -7,28 +7,35 @@
 
 void inicio (){
 	printf ("========================================");
-	printf ("\n===== SAUDAÇÕES VIAJANTE DO TEMPO =====");
+	printf ("\n===== SAUDAÃ‡Ã•ES VIAJANTE DO TEMPO =====");
 	printf ("\n========================================");
 	}
 
 void fim (){
 
 	printf ("========================================");
-	printf ("\n===== ATÉ MAIS CEDO VIAJANTE DO TEMPO =====");
+	printf ("\n===== ATÃ‰ MAIS CEDO VIAJANTE DO TEMPO =====");
 	printf ("\n========================================");
 	}	
 
 
 	int main (){
-		setlocale (LC_ALL, "portuguese"); //APLICANDO A BIBLIOTECA DAS ACENTUAÇÕES
+		setlocale (LC_ALL, "portuguese"); //APLICANDO A BIBLIOTECA DAS ACENTUAÃ‡Ã•ES
 		
 		inicio ();
 		
 		char nomes [200][5]; //DECLARANDO VETOR = A QUANTIDADE DE CARACTERES E QUANTOS VARIAVEIS TEM 5
 		int i;
+		char nomeNave [200];
+		
+		
+		printf ("Sejam Muitos bem vindos");
+		printf ("Digite o nome da Nave: "); //NAVE
+		scanf ("%s",&nomeNave);
+		
 		
 		for (i=0; i<5; i++){
-			printf ("\n\nDigite o nome do %dª Piloto: ", i+1);
+			printf ("\n\nDigite o nome do %dÂª Piloto: ", i+1);
 			scanf ("%d", &nomes[i]);
 			fflush (stdin);
 		}
